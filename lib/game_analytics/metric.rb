353c343116e6936a3aeb37d6c86f151abbc11368
Jannis Widderich <j.widderich@bichinger.de>
2015-02-23 12:31:15 +0100

    reformatting

--- lib/game_analytics/metric.rb
+++ lib/game_analytics/metric.rb
@@ -1,61 +1,61 @@
 module GameAnalytics
-class Metric
+  class Metric
 
-  include Common
+    include Common
 
-  attr_accessor :origin_ip
+    attr_accessor :origin_ip
 
-  def self.new_with_ip(ip, data={})
-    metric = self.new(data)
-    metric.origin_ip = ip
-    metric
-  end
-
-  def initialize(data={})
-    @data = data
-    needs = required_keys - data.keys
-    raise "missing required fields #{needs}" unless needs.empty?
-  end
-
-  def as_json(options={})
-    @data
-  end
-
-  def required_keys
-    [:user_id, :session_id, :build, :event_id]
-  end
-
-
-  class Design < Metric
-  end
-
-  class User < Metric
-  end
-
-  class Business < Metric
-
-    def required_keys
-      super + [:currency, :amount]
+    def self.new_with_ip(ip, data={})
+      metric = self.new(data)
+      metric.origin_ip = ip
+      metric
     end
-
-  end
-
-  class Quality < Metric
 
     def initialize(data={})
-      super
-      logger.warn('Deprecation Warning: the Quality metric is deprecated, please use the Error metric instead')
+      @data = data
+      needs = required_keys - data.keys
+      raise "missing required fields #{needs}" unless needs.empty?
     end
 
-  end
-
-  class Error < Metric
+    def as_json(options={})
+      @data
+    end
 
     def required_keys
-      super - [:event_id]
+      [:user_id, :session_id, :build, :event_id]
+    end
+
+
+    class Design < Metric
+    end
+
+    class User < Metric
+    end
+
+    class Business < Metric
+
+      def required_keys
+        super + [:currency, :amount]
+      end
+
+    end
+
+    class Quality < Metric
+
+      def initialize(data={})
+        super
+        logger.warn('Deprecation Warning: the Quality metric is deprecated, please use the Error metric instead')
+      end
+
+    end
+
+    class Error < Metric
+
+      def required_keys
+        super - [:event_id]
+      end
+
     end
 
   end
-
-end
 end
--- lib/game_analytics/worker.rb
+++ lib/game_analytics/worker.rb
@@ -2,37 +2,37 @@ require 'digest/md5'
 require 'httpclient'
 
 module GameAnalytics
-class Worker
-  
-  include Common
+  class Worker
+
+    include Common
 
 
-  def initialize(q)
-    @queue = q
-    @http = HTTPClient.new
-    @url_base = "http://api.gameanalytics.com/1/#{options[:game_key]}"
-  end
-  
-  def process(unit)
-    metric = unit.is_a?(Array) ? unit.first : unit
-    klass = metric.class
-
-    json_data = unit.to_json
-    header = {'Authorization' => Digest::MD5.hexdigest(json_data + options[:secret_key])}
-    header['X-Forwarded-For'] = metric.origin_ip if metric.origin_ip
-    category = klass.name.demodulize.downcase
-    url = "#{@url_base}/#{category}"
-    logger.info "GameAnalytics <: #{url} #{json_data} #{header.inspect}"
-    resp = @http.post(url, :body => json_data, :header => header)
-    logger.info "GameAnalytics >: #{resp.content} (#{resp.status})"
-  end
-  
-  def run
-    logger.info "GameAnalytics worker running"
-    loop do
-      process @queue.pop
+    def initialize(q)
+      @queue = q
+      @http = HTTPClient.new
+      @url_base = "http://api.gameanalytics.com/1/#{options[:game_key]}"
     end
-  end
 
-end
+    def process(unit)
+      metric = unit.is_a?(Array) ? unit.first : unit
+      klass = metric.class
+
+      json_data = unit.to_json
+      header = {'Authorization' => Digest::MD5.hexdigest(json_data + options[:secret_key])}
+      header['X-Forwarded-For'] = metric.origin_ip if metric.origin_ip
+      category = klass.name.demodulize.downcase
+      url = "#{@url_base}/#{category}"
+      logger.info "GameAnalytics <: #{url} #{json_data} #{header.inspect}"
+      resp = @http.post(url, :body => json_data, :header => header)
+      logger.info "GameAnalytics >: #{resp.content} (#{resp.status})"
+    end
+
+    def run
+      logger.info "GameAnalytics worker running"
+      loop do
+        process @queue.pop
+      end
+    end
+
+  end
 end
